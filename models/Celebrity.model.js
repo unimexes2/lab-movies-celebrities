8b3ede224d1ac4dcff5ebce976e087d03fc03af2
unimexes2 <unimexes2@gmail.com>
2022-06-28 14:41:03 +0200

    2 iter

--- models/Celebrity.model.js
+++ models/Celebrity.model.js
@@ -1,1 +1,19 @@
 //  Add your code here
+const mongoose = require("mongoose");
+
+const celebSchema = new mongoose.Schema({
+    name: {
+        type: String
+    },
+    occupation : {
+        type: String
+
+    },
+    catchPhrase: {
+        type: String
+      }
+});
+
+const Celeb = mongoose.model("Celebrity", celebSchema);
+
+module.exports = Celeb;
